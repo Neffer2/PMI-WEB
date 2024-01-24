bd36cf840994ae84e694c7d2628b107d5160aeff
Desarrollador <Desarrollador@BULLM.LOC>
2024-01-24 18:16:08 -0500

    Avances modulo Ejecucion Actividad

--- app/Livewire/Modulos/Ejecucion.php
+++ app/Livewire/Modulos/Ejecucion.php
@@ -3,11 +3,94 @@
 namespace App\Livewire\Modulos;
 
 use Livewire\Component;
+use App\Models\Punto;
+use Livewire\WithFileUploads; 
 
 class Ejecucion extends Component
 {
+    use WithFileUploads; 
+    // $this->remision->store('public/remisiones'); 
+
+    // MODELS
+    public $punto, $fecha, $estrato, $barrio, $marca_foco, $selfie_pdv, $foto_fachada;
+
+    // Useful vars
+    public $puntos = [];
+
     public function render()
     {
         return view('livewire.modulos.ejecucion');
+    }
+
+    public function mount(){
+
+    }
+
+    public function getPunto(){
+        $this->puntos = Punto::select('id', 'descripcion')->get();
+    }
+
+    public function store(){
+
+        $this->validate([
+            'punto' => 'required|numeric',
+            'fecha' => 'required|date',
+            'estrato' => 'required|numeric',
+            'barrio' => 'required|string|max:200',
+            'marca_foco' => 'required|boolean',
+            'selfie_pdv' => 'required|mimes:jpeg,png,jpg,gif',
+            'foto_fachada' => 'required|mimes:jpeg,png,jpg,gif'
+        ]);
+        
+        // dd($this->punto);
+        // dd($this->fecha);
+        // dd($this->estrato);
+        // dd($this->barrio);
+        // dd($this->marca_foco);
+        // dd($this->selfie_pdv);
+        // dd($this->foto_fachada);
+    }
+
+    // UPDATES
+    public function updatedPunto(){
+        $this->validate([
+            'punto' => 'required|numeric'
+        ]);
+    }
+
+    public function updatedFecha(){
+        $this->validate([
+            'fecha' => 'required|date'
+        ]);
+    }
+
+    public function updatedEstrato(){
+        $this->validate([
+            'estrato' => 'required|numeric'
+        ]);
+    }
+
+    public function updatedBarrio(){
+        $this->validate([
+            'barrio' => 'required|string|max:200'
+        ]);
+    }
+
+    public function updatedMarcaFoco(){
+        $this->validate([
+            'marca_foco' => 'required|boolean'
+        ]);
+    }
+
+    public function updatedSelfiePdv(){
+        $this->validate([
+            'selfie_pdv' => 'required|mimes:jpeg,png,jpg,gif'
+        ]);
+    }
+    
+    public function updatedFotoFachada(){
+        $this->validate([
+            'foto_fachada' => 'required|mimes:jpeg,png,jpg,gif'
+        ]);
     }
 }
--- resources/views/livewire/modulos/ejecucion.blade.php
+++ resources/views/livewire/modulos/ejecucion.blade.php
@@ -1,61 +1,123 @@
 <div class="d-flex justify-content-center">
     <div class="form-container">
-        <div class="p-2">
+        <div class="py-2">
             <div class="row">
                 <div class="col-md-6">
-                    <label for="">Punto de venta:</label>
-                    <select name="" id="" class="form-control">
+                    <label for="punto">Punto de venta:</label>
+                    <select id="punto" wire:model.lazy="punto" class="form-control">
                         <option value="">Seleccionar</option>
+                        @foreach ($puntos as $punto)
+                            <option value="{{ $punto->id }}">{{ $punto->description }}</option>
+                        @endforeach
+                        <option value="1">Punto</option>
                     </select>
+                    @error('punto')
+                        <div id="punto" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
                 <div class="col-md-6">
-                    <label for="">Fecha de la visita:</label>
-                    <input type="date" class="form-control">
+                    <label for="fecha">Fecha de la visita:</label>
+                    <input id="fecha" type="date" wire:model.lazy="fecha" class="form-control" >
+                    @error('fecha')
+                        <div id="fecha" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <label for="">Estrato:</label>
-                    <select name="" id="" class="form-control">
+                    <label for="estrato">Estrato:</label>
+                    <select id="estrato" wire:model.lazy="estrato" class="form-control">
                         <option value="">Seleccionar</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="5">6</option>
                     </select>
+                    @error('estrato')
+                        <div id="estrato" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
                 <div class="col-md-6">
-                    <label for="">Barrio:</label>
-                    <input type="text" class="form-control">
+                    <label for="barrio">Barrio:</label>
+                    <input type="text" wire:model.lazy="barrio" class="form-control">
+                    @error('barrio')
+                        <div id="barrio" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
             </div>
             <div class="row mb-2">
                 <div class="col-md-12">
-                    <label for="">¿Estaba agotada la marca foco CCs?</label>
-                    <select name="" id="" class="form-control">
+                    <label for="marca_foco">¿Estaba agotada la marca foco CCs?</label>
+                    <select id="marca_foco" wire:model.lazy="marca_foco" class="form-control">
                         <option value="">Seleccionar</option>
+                        <option value="1">Si.</option>
+                        <option value="0">No.</option>
                     </select>
+                    @error('marca_foco')
+                        <div id="marca_foco" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
             </div>
             <div class="row gy-2 mb-2">
                 <div class="col-md-12">                        
                     <div class="photo-box">
-                        <label for="inputTag">
-                            Selfie Punto de Venta
-                            <input id="inputTag" type="file" id="selfie" accept="image/*" capture="user"/><br/>
-                            <span id="imageName"></span>
-                        </label>
+                        @if ($selfie_pdv) 
+                            <img src="{{ $selfie_pdv->temporaryUrl() }}" class="photo"> 
+                        @else
+                            <label for="selfie_pdv">
+                                Selfie Punto de Venta
+                                <input id="selfie_pdv" type="file" 
+                                id="selfie_pdv" accept="image/*"
+                                wire:model.lazy="selfie_pdv" capture="user"/><br/>
+                                <span id="imageName"></span>
+                            </label>
+                        @endif
                     </div>
+                    @error('selfie_pdv')
+                        <div id="selfie_pdv" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
                 <div class="col-md-12">
                     <div class="photo-box">
-                        <label for="inputTag">
-                            Foto Fachada
-                            <input id="inputTag" type="file" id="selfie" accept="image/*" capture="user"/><br/>
-                            <span id="imageName"></span>
-                        </label>
+                        @if ($foto_fachada) 
+                            <img src="{{ $foto_fachada->temporaryUrl() }}" class="photo">
+                        @else
+                            <label for="foto_fachada">
+                                Foto Fachada
+                                <input id="foto_fachada" type="file"
+                                id="foto_fachada" accept="image/*"
+                                wire:model="foto_fachada" capture="user"
+                                x-on:click="isOpen"/><br/>
+                                <span id="imageName"></span>
+                            </label>
+                        @endif
                     </div>
+                    @error('foto_fachada')
+                        <div id="foto_fachada" class="text-invalid">
+                            {{ $message }}
+                        </div>   
+                    @enderror
                 </div>
             </div>
             <div class="row gy-2">
                 <div class="col-md-12">
-                    <button class="btn btn-primary w-100">ENVIAR</button>
+                    <button wire:click="store"
+                    wire:confirm="¿Deseas enviar el módulo Ejecución de la Actividad?"
+                    class="btn btn-primary w-100">ENVIAR</button>
                 </div>
                 <div class="col-md-12">
                     <button class="btn btn-secondary w-100">VOLVER</button>
@@ -64,3 +126,4 @@
         </div>
     </div>
 </div>
+ 
