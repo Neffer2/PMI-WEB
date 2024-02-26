0a09c28830caae966c41d574a81e8a2c439aa6ca
Neffer2 <n3f73r@gmail.com>
2024-02-25 21:01:36 -0500

    1. Corrección modelo minúscula (IA)

--- app/Livewire/Admin/Edit.php
+++ app/Livewire/Admin/Edit.php
@@ -4,7 +4,7 @@ namespace App\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Venta;
-use App\Models\producto;
+use App\Models\Producto;
 use Illuminate\Support\Facades\Auth;
 use App\Models\EjecucionActividad;
  
