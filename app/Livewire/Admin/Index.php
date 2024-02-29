d9b2e8d81fa566e86dfdcc03fbbc23fc9f9ac026
Neffer <n3f73r@gmail.com>
2024-02-29 09:37:14 -0500

    Filtro x ciudad funcional

--- app/Livewire/Admin/Index.php
+++ app/Livewire/Admin/Index.php
@@ -5,15 +5,36 @@ namespace App\Livewire\Admin;
 use Livewire\WithPagination;
 use Livewire\Component;
 use App\Models\EjecucionActividad;
+use App\Models\Ciudad;
 
 class Index extends Component
 {
     use WithPagination;
+    // Models 
+    public $ciudad;
+
+    // Useful vars
+    public $ciudades = [], $superAdmin = false;
 
     public function render()
     {
-        $ejecuciones = EjecucionActividad::select('id', 'user_id', 'fechaVisita', 'punto_id')->orderBy('id', 'desc')->paginate(15);
+        $filtro = [];
+        if ($this->ciudad) {
+            $filtro = ['ciudad_id' => $this->ciudad];
+        }
+
+        $ejecuciones = EjecucionActividad::select('id', 'user_id', 'fechaVisita', 'punto_id')
+                    ->with('promotor')->whereHas('promotor', function ($ejecucion) use ($filtro) {
+                        $ejecucion->where($filtro);
+                    })->orderBy('id', 'desc')->paginate(15);
         return view('livewire.admin.index', ['ejecuciones' => $ejecuciones]);
     }
-}
+
+    public function mount(){
+        $this->ciudades = Ciudad::select('id', 'description')->get();
+
+        $this->superAdmin = (Auth()->user()->id == 1 || Auth()->user()->id == 3) ? true : false;
+        (!$this->superAdmin)? $this->ciudad = Auth()->user()->ciudad_id : $this->ciudad = [];
+    }
+} 
  
--- resources/views/livewire/admin/index.blade.php
+++ resources/views/livewire/admin/index.blade.php
@@ -1,40 +1,59 @@
 <div>
     <div class="container">
-        <div class="card mt-4 mb-1">
-            <div class="card-body">
-                <div class="table-responsive" style="max-height: none">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Promotor</th>
-                                <th>Ciudad</th>
-                                <th>Punto</th>
-                                <th>Fecha visita</th>
-                                <th>Acciones</th>
-                            </tr> 
-                        </thead>
-                        <tbody>
-                            @foreach ($ejecuciones as $key => $ejecucion)
-                                <tr>
-                                    <td>{{ $key+=1 }}</td>
-                                    <td>{{ $ejecucion->promotor->name }}</td>
-                                    <td>{{ $ejecucion->promotor->ciudad->description }}</td>
-                                    <td>{{ $ejecucion->punto->descripcion }}</td>
-                                    <td>{{ $ejecucion->fechaVisita }}</td>
-                                    <td>
-                                        <a href="{{ route('visita', $ejecucion->id) }}" class="btn btn-primary">
-                                            Mas detalles
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>  
-                    </table>
+        <div class="row my-2">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="" class="bold">Ciudad:</label>
+                            <select class="form-control" wire:model.change='ciudad' @if (!$superAdmin) disabled @endif>
+                                <option value="">Seleccionar</option>
+                                @foreach ($ciudades as $ciudad)
+                                    <option value="{{ $ciudad->id }}">{{ $ciudad->description }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-12">
+                <div class="card my-2 mb-1">
+                    <div class="card-body">
+                        <div class="table-responsive" style="max-height: none">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Promotor</th>
+                                        <th>Ciudad</th>
+                                        <th>Punto</th>
+                                        <th>Fecha visita</th>
+                                        <th>Acciones</th>
+                                    </tr> 
+                                </thead> 
+                                <tbody>
+                                    @foreach ($ejecuciones as $key => $ejecucion)
+                                        <tr>
+                                            <td>{{ $key+=1 }}</td>
+                                            <td>{{ $ejecucion->promotor->name }}</td>
+                                            <td>{{ $ejecucion->promotor->ciudad->description }}</td>
+                                            <td>{{ $ejecucion->punto->descripcion }}</td>
+                                            <td>{{ $ejecucion->fechaVisita }}</td>
+                                            <td>
+                                                <a href="{{ route('visita', $ejecucion->id) }}" class="btn btn-primary">
+                                                    Mas detalles
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>  
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {{ $ejecuciones->links() }}
+            </div>
         </div>
-        {{ $ejecuciones->links() }}
     </div>
 </div>
 
